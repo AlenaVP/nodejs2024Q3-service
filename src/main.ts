e1646e86b06537f582fd6dc601138c418090928c
AlenaVP Legion <poprugo@gmail.com>
2024-11-10 15:20:54 +0100

    chore: add Swagger
    [npm install --save @nestjs/swagger swagger-ui-express]

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PORT } from '@shared/constants/env';
 
@@ -8,6 +9,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') ?? PORT;
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Home Library Service API')
+    .setDescription('Home Library Service: https://github.com/AlreadyBored/nodejs-assignments/blob/main/assignments/rest-service/assignment.md')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(port);
