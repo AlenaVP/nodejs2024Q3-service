548a73ff94cc8afe6cfa902dc41920477fd8b4bd
AlenaVP Legion <poprugo@gmail.com>
2024-11-11 10:51:02 +0100

    feat: add GlobalExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PORT } from '@shared/constants/env';
+import { GlobalExceptionFilter } from '@shared/filters/global-exception/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,6 +23,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(port);
 }
 bootstrap();
